8bcca29081959ecbfd4c783edf9a16cb38796be7
Satria Adi Putra <satadii11@gmail.com>
2019-10-18 02:02:02 +0700

    7-Loop

--- dscpertama.go
+++ dscpertama.go
@@ -3,13 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var umurGebetan = 21
-
-	if umurGebetan > 20 {
-		fmt.Println("Udah mateng")
-	} else if umurGebetan > 17 {
-		fmt.Println("Dede gemes")
-	} else {
-		fmt.Println("Buset kamu pedopil?")
+	var nilai = []int{80, 20, 10, 40, 90, 50, 70}
+	var totalNilai = 0
+	for _, value := range nilai {
+		totalNilai = totalNilai + value
 	}
+	var banyakNilai = len(nilai)
+	var ip = totalNilai / banyakNilai
+	fmt.Println("Ip saya adalah", ip)
+
+	fmt.Println("\nNilai dari for loop auto inc:")
+	for i := 0; i < banyakNilai; i++ {
+		fmt.Println(nilai[i])
+	}
+
+	fmt.Println("\nNilai dari for loop manual inc:")
+	i := 1
+	for i < banyakNilai {
+		fmt.Println(nilai[i])
+		i++
+	}
+
 }
