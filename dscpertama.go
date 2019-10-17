fe02c556c84ea74e00a889463c3dd0752240de7f
Satria Adi Putra <satadii11@gmail.com>
2019-10-18 01:25:33 +0700

    2-Variable&Constant

--- dscpertama.go
+++ dscpertama.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+var umur int = 17
+
 func main() {
-	fmt.Println("Hello World")
+	const kampus string = "Universitas Komputer Indonesia"
+	nama := "James Catalunya"
+
+	var (
+		semester = 3
+		ipk      float32
+	)
+	ipk = 6.81
+
+	fmt.Println("Saya", nama, "kuliah di", kampus, "semester", semester, "Saya berumur", umur, "tahun.")
+	fmt.Println("Ipk saya adalah ", ipk)
 }
